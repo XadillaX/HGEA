f22787e78cd72f107407d1c868d5d7c2b24323ff
XadillaX <i@233.dev>
2026-04-08 08:46:16 +0000

    feat: 分析代码和文档，确定下一步骤
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/core/random.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../include/hge.h"
-#include "hge_impl.h"
-
-// TODO: WIP
-
-void CALL HGE_Impl::Random_Seed(int seed) {}
-int CALL HGE_Impl::Random_Int(int min, int max) { return min; }
-float CALL HGE_Impl::Random_Float(float min, float max) { return min; }
--- src/core/timer.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../include/hge.h"
-#include "hge_impl.h"
-
-// TODO: WIP
-
-float CALL HGE_Impl::Timer_GetTime() { return 0.0f; }
-float CALL HGE_Impl::Timer_GetDelta() { return 0.0f; }
-int CALL HGE_Impl::Timer_GetFPS() { return 0; }
